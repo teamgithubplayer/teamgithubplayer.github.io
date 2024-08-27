a87cc5a54203827423b422908523ecf4097b1bca
teamgithubplayer <ravshan87@yahoo.com>
2024-08-27 12:00:55 +0200

    test

--- cypress/e2e/home_page.cy.js
+++ cypress/e2e/home_page.cy.js
@@ -1,5 +1,16 @@
 describe('The Home Page', () => {
   it('successfully loads', () => {
     cy.visit('https://teamgithubplayer.github.io/index.html') // change URL to match your dev URL
+    cy.get('[data-cy="head.el_1"]').click()
+    cy.get('[data-cy="head.el_2"]').click()
+    cy.get('[data-cy="head.el_3"]').click()
+    cy.get('[data-cy="head.el_3a"]').click()
+    cy.get('[data-cy="head.el_3b"]').click()
+    cy.get('[data-cy="head.el_3c"]').click()
+    cy.get('[data-cy="head.el_3d"]').click()
+    cy.get('[data-cy="head.el_4"]').click()
+    cy.get('[data-cy="head.el_5"]').click()
+    cy.get('[data-cy="head.el_6"]').click()
+    cy.get('[data-cy="head.el_7"]').click()
   })
 })
