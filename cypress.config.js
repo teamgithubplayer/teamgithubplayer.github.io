fc5038b39a60a97efc0a301861e567592579a49f
teamgithubplayer <111708648+teamgithubplayer@users.noreply.github.com>
2024-10-14 17:12:27 +0200

    data-cy

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    //baseUrl: 'https://teamgithubplayer.github.io/index.html',
+    baseUrl: 'http://localhost:8082',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/home_page.cy.js
+++ cypress/e2e/home_page.cy.js
@@ -1,16 +1,6 @@
 describe('The Home Page', () => {
   it('successfully loads', () => {
-    cy.visit('https://teamgithubplayer.github.io/index.html') // change URL to match your dev URL
+    cy.visit('')
     cy.get('[data-cy="head.el_1"]').click()
-    cy.get('[data-cy="head.el_2"]').click()
-    cy.get('[data-cy="head.el_3"]').click()
-    cy.get('[data-cy="head.el_3a"]').click({force: true})
-    cy.get('[data-cy="head.el_3b"]').click({force: true})
-    cy.get('[data-cy="head.el_3c"]').click({force: true})
-    cy.get('[data-cy="head.el_3d"]').click({force: true})
-    cy.get('[data-cy="head.el_4"]').click()
-    cy.get('[data-cy="head.el_5"]').click()
-    cy.get('[data-cy="head.el_6"]').click()
-    cy.get('[data-cy="head.el_7"]').click()
   })
 })
