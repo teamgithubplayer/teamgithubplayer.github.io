ff0a5a67b5f8cc957a44935b726180e94b4baca0
teamgithubplayer <111708648+teamgithubplayer@users.noreply.github.com>
2024-11-05 12:26:04 +0100

    upersonalize, testcases

--- cypress/e2e/home_page.cy.js
+++ cypress/e2e/home_page.cy.js
@@ -1,6 +1,33 @@
-describe('The Home Page', () => {
-  it('successfully loads', () => {
+describe('My Page', () => {
+  it('Main Page', () => {
     cy.visit('')
-    cy.get('[data-cy="head.el_1"]').click()
+    cy.get('[data-cy="p1.el_3"]').click()
+    cy.url().should('include', '/Page3/3rdPage.html')
+  })
+  it('Page2', () => {
+    cy.visit('/Page2/2ndPage.html')
+    cy.get('[data-cy="p2.el_2"]').click()
+    cy.get('[data-cy="p2.slide_1_3"]').should('be.visible')
+    cy.get('[data-cy="p2.el_1"]').click()
+    cy.get('[data-cy="p2.slide_1_2"]').should('be.visible')
+  })
+  it('Page3', () => {
+    cy.visit('/Page3/3rdPage.html')
+    cy.get('[data-cy="p2.el_2"]').click()
+  })
+  it('Page3a', () => {
+    cy.visit('/Page3/Page3a/3rdPageA.html')
+  })
+  it('Page3b', () => {
+    cy.visit('/Page3/Page3b/3rdPageB.html')
+  })
+  it('Page3c', () => {
+    cy.visit('/Page3/Page3c/3rdPageC.html')
+  })
+  it('Page3d', () => {
+    cy.visit('/Page3/Page3d/3rdPageD.html')
+  })
+  it('Page4', () => {
+    cy.visit('/Page4/4thPageD.html')
   })
 })
